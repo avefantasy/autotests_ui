fba2e6c7d9dcffca604edb05fe27f79ae3c3955f
avefantasy <artemkor111@yandex.ru>
2025-10-03 13:30:06 +0300

    Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,28 +1,21 @@
 import pytest
-from playwright.sync_api import expect, Page
+from pages.login_page import LoginPage
 
 @pytest.mark.regression
 @pytest.mark.authorization
 @pytest.mark.parametrize("email, password", [("user.name@gmail.com", "password"),
                                             ("user.name@gmail.com", "  "),
                                             ("  ", "password")])
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+
     # Переход на страницу входа
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    # Заполнение email
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
-
-    # Заполнение password
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
+    # Заполнение email и password
+    login_page.fill_login_form(email=email, password=password)
 
     # Нажатие кнопки Login
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
+    login_page.click_login_button()
 
     # Проверка сообщения об ошибке
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    login_page.check_visible_wrong_email_or_password_alert()
