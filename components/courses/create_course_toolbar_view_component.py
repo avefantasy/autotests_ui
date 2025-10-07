54c4485f1e13e171c33be463eb1f556c0dae12d1
avefantasy <artemkor111@yandex.ru>
2025-10-07 19:01:43 +0300

    Практикуемся в реализации PageComponent.Практикуемся в написании компонентов с использованием паттерна PageComponent

--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -9,13 +9,14 @@ class CreateCourseToolbarViewComponent(BaseComponent):
         self.title = page.get_by_test_id('create-course-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
 
-    def check_visible(self, is_create_course_disabled= True):
+    def check_visible(self, is_create_course_disabled: bool = True):
 
         expect(self.title).to_be_visible()
         expect(self.title).to_have_text('Create course')
 
         if is_create_course_disabled:
             expect(self.create_course_button).to_be_disabled()
+
         if not is_create_course_disabled:
             expect(self.create_course_button).to_be_enabled()
 
