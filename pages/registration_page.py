0a486c3092f626a4e6a07c8e6a49b648557ced3c
avefantasy <artemkor111@yandex.ru>
2025-10-08 11:12:45 +0300

    Знакомство с PageFactory.Практика работы с Page Factory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
 
 # Page Object для страницы регистрации
 class RegistrationPage(BasePage):
@@ -11,7 +12,7 @@ class RegistrationPage(BasePage):
         # Компонент для заполнения формы регистрации и проверки корректности данных
         self.registration_form = RegistrationFormComponent(page)
 
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+        self.registration_button = Button(page,'registration-page-registration-button', 'Registration')
 
     # Нажатие кнопки регистрации
     def click_registration_button(self):
