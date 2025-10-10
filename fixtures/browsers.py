f3240eb16e239e27f2778b369810b8baf21e05fe
avefantasy <artemkor111@yandex.ru>
2025-10-10 23:26:15 +0300

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -5,6 +5,7 @@ from _pytest.fixtures import SubRequest
 
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
+from config import settings
 
 # Фикстура открытия и закрытия браузера
 @pytest.fixture
@@ -14,24 +15,20 @@ def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
 # Фикстура регистрация нового пользователя и сохранение авторизованного состояния
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-    # Открытие браузера и создание страницы
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=settings.headless)  # Используем settings.headless
     context = browser.new_context()
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
-    # Переход на страницу регистрации
-    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-    # Заполнение email
-    # Заполнение username
-    # Заполнение password
-    # Нажатие на кнопку registration
-    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,  # Используем settings.test_user.email
+        username=settings.test_user.username,  # Используем settings.test_user.username
+        password=settings.test_user.password  # Используем settings.test_user.password
+    )
     registration_page.click_registration_button()
 
-    # Сохранение авторизованного состояния в json файл
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path=settings.browser_state_file)  # Используем settings.browser_state_file
     browser.close()
 
 # Фикстура авторизованной сессии
@@ -40,5 +37,5 @@ def chromium_page_with_state(initialize_browser_state, request: SubRequest, play
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state="browser-state.json"
+        storage_state=settings.browser_state_file  # Используем settings.browser_state_file
     )
