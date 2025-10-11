0375b45741a53e7c9fb4589150b6afca94b296d4
avefantasy <artemkor111@yandex.ru>
2025-10-11 17:05:52 +0300

    Логирование взаимодействий со страницей.Практика работы с логированием

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import Page, expect
 from typing import Pattern
 import allure
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
 
 class BasePage:
     # Конструктор класса, принимающий объект Page
@@ -8,14 +11,20 @@ class BasePage:
         self.page = page
 
     def visit(self, url: str):  # Метод для открытия ссылок
-        with allure.step(f'Opening the url "{url}"'):
+        step = f'Opening the url "{url}"'
+        with allure.step(step):
             self.page.goto(url, wait_until='networkidle')
+            logger.info(step)
 
     def reload(self):  # Метод для перезагрузки страницы
-        with allure.step(f'Reloading page with url "{self.page.url}"'):
+        step = f'Reloading page with url "{self.page.url}"'
+        with allure.step(step):
             self.page.reload(wait_until='domcontentloaded')
+            logger.info(step)
 
     # Метод для проверки текущего URL
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+        with allure.step(step):
             expect(self.page).to_have_url(expected_url)
+            logger.info(step)
