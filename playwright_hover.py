61604d2e3144931375ee8a176b2a1a82c2ea0eb4
avefantasy <artemkor111@yandex.ru>
2025-10-13 18:10:33 +0300

    Запуск автотестов на CI.Практика работы с GitHub Actions

--- playwright_hover.py
+++ playwright_hover.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
     #Открытие браузера и создание страницы
